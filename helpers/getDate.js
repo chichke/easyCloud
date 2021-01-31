68c92a642f71e48bbc4e146e7ffeb568bb1e295f
Bastien Silhol <bastien.silhol@epitech.eu>
2021-01-31 14:43:42 +0100

    Fixed release momentJs

--- helpers/getDate.js
+++ helpers/getDate.js
@@ -1,5 +1,6 @@
 import * as Localization from 'expo-localization';
 import moment from 'moment';
+import 'moment-timezone';
 import 'moment/min/locales';
 
 const getDate = (date) => moment(date).locale(Localization.locale).fromNow();
