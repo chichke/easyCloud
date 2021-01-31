6684923eb571a22e3eb04c3ac96fbacebb8a27f3
Bastien Silhol <bastien.silhol@epitech.eu>
2021-01-31 14:24:15 +0100

    Trying to fix error on release build

--- helpers/getDate.js
+++ helpers/getDate.js
@@ -1,7 +1,6 @@
 import * as Localization from 'expo-localization';
 import moment from 'moment';
-import 'moment/locale/fr';
-import 'moment/locale/it';
+import 'moment/min/locales';
 
 const getDate = (date) => moment(date).locale(Localization.locale).fromNow();
 
